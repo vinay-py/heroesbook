50c605fef2114b53fcdbb09f228a118b7616b21c
Vinay <vinay.py@gmail.com>
2021-03-15 18:59:25 -0400

    Initial COmmit

---
+++ src/main/java/com/example/heroesbook/HeroesbookApplication.java
@@ -0,0 +1,13 @@
+package com.example.heroesbook;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HeroesbookApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HeroesbookApplication.class, args);
+	}
+
+}
